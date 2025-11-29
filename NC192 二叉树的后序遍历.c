78091bce3c80902dbc3597165528bc03e18ae9a2
qhe32673-arch <qhe32673@gmail.com>
2025-11-29 17:59:27 +0800

    树 递归 广度优先搜索(BFS)

---
+++ NC192 二叉树的后序遍历.c
@@ -0,0 +1,27 @@
+void fuc(struct TreeNode* root, int* arr,int * count)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    fuc(root->left, arr,count);
+    fuc(root->right, arr,count);
+    
+    arr[*count] = root->val;
+    //printf("%d ",arr[*count]);
+    //printf("%d\n",*count);
+    (*count)++;
+}
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize ) {
+    int* arr = (int*)malloc(sizeof(4)*1000);
+    int count = 0;
+    fuc(root, arr,&count);
+    //while(*arr)
+    //{
+        //printf("%d ",*arr);
+        //arr++;
+    //}
+    *returnSize=count;
+    return arr;
+}
