1a1917e932ac47032e8bce1040581635f0063fd9
枫叶c <2744235537@qq.com>
2025-12-04 13:39:41 +0800

    数组 排序

---
+++ B-TO52 调整牛群的位置.c
@@ -0,0 +1,39 @@
+int* rotate_left(int* nums, int numsLen, int k, int* returnSize ) {
+    * returnSize=numsLen;
+    k%=numsLen;
+    if(k==0)
+    {
+        return nums;
+    }
+    int left = numsLen-k;
+    int right = numsLen-1;
+    while(left<right)
+    {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right]=tmp;
+        left++;
+        right--;
+    }
+    left = 0;
+    right = numsLen-k-1;
+    while(left<right)
+    {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right]=tmp;
+        left++;
+        right--;
+    }
+    left = 0;
+    right = numsLen-1;
+    while(left<right)
+    {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right]=tmp;
+        left++;
+        right--;
+    }
+    return nums;
+}
