3307186298a3a52119024596c865e706999732ee
枫叶c <2744235537@qq.com>
2025-12-09 23:24:26 +0800

    模拟

---
+++ BC12 字符圣诞树.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    char a = 0;
+    scanf("%c",&a);
+    printf("    %c\n",a);
+    printf("   %c %c\n",a,a);
+    printf("  %c %c %c\n",a,a,a);
+    printf(" %c %c %c %c\n",a,a,a,a);
+    printf("%c %c %c %c %c",a,a,a,a,a);
+    
+    return 0;
+}
