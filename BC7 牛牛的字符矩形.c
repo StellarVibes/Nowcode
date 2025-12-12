1812e4d08c0ff4ac480a2a3b301c8a9c5599adf9
枫叶c <2744235537@qq.com>
2025-12-13 01:18:15 +0800

    模拟

---
+++ BC7 牛牛的字符矩形.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	char i = 0;
+	scanf("%c", &i);
+	printf("%c%c%c\n", i,i,i);
+	printf("%c%c%c\n", i,i,i);
+	printf("%c%c%c\n", i,i,i);
+    
+	return 0;
+}
