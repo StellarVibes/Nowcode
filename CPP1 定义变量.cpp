e405d57c3902f9eaeb685b7ea2549e9344cb60f1
枫叶c <2744235537@qq.com>
2025-12-16 00:51:51 +0800

    语法

---
+++ CPP1 定义变量.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(double)<<endl;
+    
+    return 0;
+}
