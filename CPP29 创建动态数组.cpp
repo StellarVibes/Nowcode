361c55352bca49ae43f95a5c5fa904231bbfcfc9
枫叶c <2744235537@qq.com>
2025-12-17 19:58:26 +0800

    数组

---
+++ CPP29 创建动态数组.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int * a = new int[n];
+    for(int i = 0;i<n;i++)
+    {
+        a[i]=n+i;
+    }
+    for(int i = 0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
