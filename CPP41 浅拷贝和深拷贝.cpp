d99a38d0487cf28cba9a8c863c9be6d43241bca9
枫叶c <2744235537@qq.com>
2025-12-18 23:36:40 +0800

    语法 数组 结构体

---
+++ CPP41 浅拷贝和深拷贝.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#pragma warning(disable : 4996)
+using namespace std;
+
+class Person {
+
+    public:
+        char* name; // 姓名
+        int age;    // 年龄
+        Person(const char* name, int age) 
+        {
+            this->name = new char[strlen(name) + 1];
+            strcpy(this->name, name);
+            this->age = age;
+        }
+        Person (Person& p)
+        {
+            this->name = new char[strlen(name) + 1];
+            strcpy(this->name, p.name);
+            this->age=p.age;
+        }
+        void showPerson() 
+        {
+            cout << name << " " << age << endl;
+        }
+        ~Person() 
+        {
+            if (name != nullptr) 
+            {
+                delete[] name;
+                name = nullptr;
+            }
+        }
+};
+
+int main() {
+    char name[100] = { 0 };
+    int age;
+    cin >> name;
+    cin >> age;
+    Person p1(name, age);
+    Person p2 = p1;
+    p2.showPerson();
+
+    return 0;
+}
