fa3e6f9de15d9cf76f5fb6582211768f1db7111d
枫叶c <2744235537@qq.com>
2025-12-18 23:38:19 +0800

    语法

---
+++ CPP6 交换两个变量的值.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin >> b;
+    swap(a,b);
+    cout << a << " " << b << endl;
+    
+    return 0;
+}
