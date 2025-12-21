deb7d622652ddac59036a48604c1ca4e856ee393
枫叶c <2744235537@qq.com>
2025-12-22 00:07:20 +0800

    语法

---
+++ ST-C27 逆序输出.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 0,b = 0,c = 0,d = 0,e = 0,f = 0,g = 0,h = 0,i = 0,j = 0;
+	scanf("%d%d%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+	printf("%d %d %d %d %d %d %d %d %d %d", j, i, h, g, f, e, d, c, b, a);
+    
+	return 0;
+}
