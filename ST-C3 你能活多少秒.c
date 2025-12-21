3c656ec4056c0890dc45fa26dba38e1941e53844
枫叶c <2744235537@qq.com>
2025-12-22 00:07:29 +0800

    数学

---
+++ ST-C3 你能活多少秒.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	long int a = 0;
+	scanf("%d", &i);
+	a = i * 31560000;
+	printf("%ld", a);
+    
+	return 0;
+}
