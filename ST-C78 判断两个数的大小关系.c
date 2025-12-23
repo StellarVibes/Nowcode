b3049ce2ee913f96869d3a414c08ab175cc59334
枫叶c <2744235537@qq.com>
2025-12-24 01:56:23 +0800

    数学

---
+++ ST-C78 判断两个数的大小关系.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	while (scanf("%d %d", &a, &b)!= -1)
+	{
+		if (a > b)
+			printf("%d>%d\n", a, b);
+		else if (a == b)
+			printf("%d=%d\n", a, b);
+		else
+			printf("%d<%d\n", a, b);
+	}
+
+	return 0;
+}
